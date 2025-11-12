73686e63f3b3732cbf06670b28264daf6e0cf267
Nasdaldi <anugrahwiradhikanasdaldi@gmail.com>
2025-11-12 11:34:07 +0700

    jobseet10

--- joobsheet9/ArrayBilangan18.java
+++ joobsheet9/ArrayBilangan18.java
@@ -1,11 +1,7 @@
 package joobsheet9;
 public class ArrayBilangan18 {
     public static void main(String[] args) {
-        int[] bilangan = new int [4];
-        bilangan[0] = 5;
-        bilangan[1] = 13;
-        bilangan[2] = -7;
-        bilangan[3] = 17;
+        int[] bilangan = {5, 13, -7, 17};      
         for (int i = 0; i < 4 ; i++) {
             System.out.println(bilangan [i]);
         }
